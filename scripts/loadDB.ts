cf0d4af5c945c1aa4001e37fc5eb3f4701024407
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-12-24 13:02:44 -0500

    add dependencies for scripts, add .env

--- scripts/loadDB.ts
+++ scripts/loadDB.ts
@@ -1,9 +1,5 @@
-// Load all data into AstraDB and do our web scraping
+import "dotenv/config"
 
 import { DataAPIClient } from "@datastax/astra-db-ts"
-import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer"
-
-// for vector embeddings instead of openai use gemini
 import { GoogleGenerativeAI } from "@google/generative-ai"
-
 
