7fe04bc70651aecb47af43bd144e7dedbf995d22
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-12-25 18:54:48 -0500

    update loadingbubble component

--- pages/components/LoadingBubble.tsx
+++ pages/components/LoadingBubble.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./LoadingBubble.css"; 
+import styles from "./LoadingBubble.module.css"; // Import the CSS Module
 
 const LoadingBubble = () => {
   return (
-    <div className="loading-container">
-      <span className="loading-span blur-5"></span>
-      <span className="loading-span blur-10"></span>
-      <span className="loading-span blur-25"></span>
-      <span className="loading-span blur-50"></span>
+    <div className={styles["loading-container"]}>
+      <span className={styles["loading-span"] + " " + styles["blur-5"]}></span>
+      <span className={styles["loading-span"] + " " + styles["blur-10"]}></span>
+      <span className={styles["loading-span"] + " " + styles["blur-25"]}></span>
+      <span className={styles["loading-span"] + " " + styles["blur-50"]}></span>
     </div>
   );
 };
