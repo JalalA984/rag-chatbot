3f118643cd4e17980c9570d380157627835a8773
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-12-25 18:49:21 -0500

    change build dscript

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack(config) {
+    // Adding CSS support if necessary
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
-
-
