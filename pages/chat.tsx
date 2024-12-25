7a17c4e6e4026763f354e824459e109b75fe2296
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-12-25 12:00:18 -0500

    setup chat page, no styling yet

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 import SupernovaImage from "@/public/supernova.svg";
 import React from "react";
+import { useChat } from "ai/react";
+import { Message } from "ai";
 
 const chat = () => {
-  const noMessages = true;
+  const {
+    append,
+    isLoading,
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+  } = useChat();
+
+  const noMessages = false;
 
   return (
     <main>
@@ -11,11 +22,29 @@ const chat = () => {
         src={SupernovaImage}
         alt="DevHorizon Nova logo"
         width={240}
-        className="transition-transform duration-500 hover:scale-110 hover:rotate-6 hover:opacity-90 -ml-6"
+        className="transition-transform duration-500 hover:scale-110 hover:rotate-6 hover:opacity-90"
       />
-      <section>
-        {noMessages ? <p>No messages yet</p> : <p> we have messages </p>}
+      <section className={noMessages ? "" : ""}>
+        {noMessages ? (
+          <>
+            <p>Ask DevHorizon Nova about anything tech! We hope you enjoy!</p>
+            <br />
+          </>
+        ) : (
+          <>
+            <p>we have messages</p>
+          </>
+        )}
       </section>
+      <form onSubmit={handleSubmit}>
+          <input
+            className=""
+            onChange={handleInputChange}
+            value={input}
+            placeholder="Ask me anything..."
+          />
+          <input type="submit" className="" />
+        </form>
     </main>
   );
 };
